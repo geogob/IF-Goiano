362ebc5e9b2040b4d169d4cca754ee35595beefb
George Oliveira Barros <geogobgob@gmail.com>
2017-09-09 19:56:44 -0300

    Organizando diretórios

---
+++ Desenvolvimento WEB III/segunda.php
@@ -0,0 +1,32 @@
+<?php
+
+//1
+//Objeto Date
+echo "Hoje	é	dia	"	.	date('d/m/y');
+echo "	agora	são	"	.	date('H:i:s');
+
+echo "Faltam". (7 - date('w')). "dias para sábado" ;
+
+//Atualize pra ver a mudança!
+
+//2 Desafios!
+
+//3 Calendário
+/*?>
+<table	border="1">
+				<tr>
+								<th>Dom</th>
+								<th>Seg</th>
+								<th>Ter</th>
+								<th>Qua</th>
+								<th>Qui</th>
+								<th>Sex</th>
+								<th>Sáb</th>
+				</tr>
+</table>
+<?php*/
+
+/*4 Exemplo com php dentro do html e inspecionar elemento<h1><?php	echo "Título	dentro	do	H1";?></h1>*/
+//5 Hoje.php
+
+?>
