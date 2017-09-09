8d0b55a44ee65b6e8993a1ec94abce05cffc69b0
George Oliveira Barros <geogobgob@gmail.com>
2017-09-09 20:08:25 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	define('DB_HOSTNAME', 'localhost'); //Nome de HOST
-	define('DB_USERNAME', 'root'); //Usuário
-	define('DB_PASSWORD', null); //Senha
-	define('DB_DATABASE', 'aula'); //Nome do BD
-	//define('DB_PREFIX', 'dw3');
-	define('DB_CHARSET', 'utf8');	
-?>
