aad6ace6a32067332b213e5d9ba029aaf2c0f6b1
George Oliveira Barros <geogobgob@gmail.com>
2017-09-09 19:46:49 -0300

    CRUD PHP MySQL - Conexão e Inserção

---
+++ hoje.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Dia	<?php	echo	date('d/m/Y');	?></title>
+	</head>
+			<body>
+					<h1>
+						Estamos	em	<?php	echo	date('Y');	?>	e	hoje	é	dia
+						<?php	echo	date('d/m');	?>
+					</h1>
+					<p>
+						Esta	página	foi	gerada	às	<?php	echo	date('H');	?>
+						horas	e	<?php	echo	date('i');	?>	minutos.
+					</p>
+			</body>
+<html>
---
+++ primeiro.php
@@ -0,0 +1,48 @@
+<?php
+
+$n = 15;
+
+for ($i=0; $i < $n ; $i++) { 
+	echo "*";
+}
+
+/*
+$op=1;
+switch ($op) {
+	case 1:
+		echo "valor 1";
+		break;
+	case 2:
+		echo "valor 2";
+		break;
+	default:
+		echo "Nem um nem outro";
+		break;
+}
+
+for ($i=0; $i < 100; $i++) { 
+	echo $i;
+}*/
+
+//AULA
+//Impressão
+//Concatenar variavel.
+//Variáveis e seus tipos $x = "maldade";
+//Constantes
+//Estruturas condicionais
+//Estruturas de repetição
+//Funções
+//
+
+//Questão dos asteriscos
+//Questão dos asteriscos pirâmide
+/*
+$array = array("tes","oi",9);
+var_dump($array);
+
+Acessando a data
+echo "Hoje	é	dia	"	.	date('d/m/Y');
+echo "	agora	são	"	.	date('H:i:s');
+
+*/
+?>
