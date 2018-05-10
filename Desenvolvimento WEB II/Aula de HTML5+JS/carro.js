e8b8e79923fa33eadb7a0d1154a1dd6d43d66920
geogob <geogobgob@gmail.com>
2018-05-09 23:21:38 -0300

    update animation

---
+++ Desenvolvimento WEB II/Aula de HTML5+JS/carro.js
@@ -0,0 +1,14 @@
+//	arquivo:	carro.js
+//	Função	construtora
+function	Carro(cor,	velocMaxima){
+			this.cor	=	cor;
+			this.velocMaxima	=	velocMaxima;
+			this.velocAtual	=	0;
+}
+
+//	Prototype	com	os	métodos
+Carro.prototype	= {
+			acelerar: function() {
+						this.velocAtual	+=	10;
+			}
+}
