84c9249fce38befa5e17009eee86f790c9ff9b9e
geogob <geogobgob@gmail.com>
2018-06-08 08:53:01 -0300

    spritesheets

--- Desenvolvimento WEB II/Aula de HTML5+JS/animacao.js
+++ Desenvolvimento WEB II/Aula de HTML5+JS/animacao.js
@@ -1,44 +1,42 @@
-function Animacao(context)	{	
-		this.context	=	context;	
-		this.sprites	=	[];	
-		this.ligado	=	false
-}	
+function Animacao(context) {
+   this.context = context;
+   this.sprites = [];
+   this.ligado = false;
+}
+Animacao.prototype = {
+   novoSprite: function(sprite) {
+      this.sprites.push(sprite);
+   },
+   ligar: function() {
+      this.ligado = true;
+      this.proximoFrame();
+   },
+   desligar: function() {
+      this.ligado = false;
+   },
+   proximoFrame: function() {
+      // Posso continuar?
+      if ( ! this.ligado ) return;
 
-Animacao.prototype	=	{	
-			
-			novoSprite:	function(sprite)	{	
-						this.sprites.push(sprite);	
-			},	
-			
-			ligar:	function()	{	
-						this.ligado	=	true;	
-						this.proximoFrame();
-			},		
-			
-			desligar:	function()	{	
-						this.ligado	=	false;
-			},
-			
-			limparTela:	function()	{	
-					var	ctx	=	this.context;		//	Só	para	facilitar	a	escrita	;)
-					ctx.clearRect(0,	0,	ctx.canvas.width,	ctx.canvas.height);	
-			},
-			
-			proximoFrame:	function()	{	
-						//	Posso	continuar?	
-						if	(	!	this.ligado	)	return;	
-						//	A	cada	ciclo,	limpamos	a	tela	ou	desenhamos	um	fundo	
-						this.limparTela();	
-						//	Atualizamos	o	estado	dos	sprites	
-						for	(var	i	in	this.sprites)	
-									this.sprites[i].atualizar();	
-						//	Desenhamos	os	sprites	
-						for	(var	i	in	this.sprites)	
-									this.sprites[i].desenhar();	
-						//	Chamamos	o	próximo	ciclo	
-						var	animacao	=	this;	
-						requestAnimationFrame(function()	{	
-									animacao.proximoFrame();	
-						});
-			}
+      // A cada ciclo, limpamos a tela ou desenhamos um fundo
+      this.limparTela();
+
+      // Atualizamos o estado dos sprites
+      for (var i in this.sprites)
+         this.sprites[i].atualizar();
+
+      // Desenhamos os sprites
+      for (var i in this.sprites)
+         this.sprites[i].desenhar();
+
+      // Chamamos o próximo ciclo
+      var animacao = this;
+      requestAnimationFrame(function() {
+         animacao.proximoFrame();
+      });
+   },
+   limparTela: function() {
+      var ctx = this.context;
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+   }
 }
--- Desenvolvimento WEB II/Aula de HTML5+JS/teclado.js
+++ Desenvolvimento WEB II/Aula de HTML5+JS/teclado.js
@@ -1,42 +1,44 @@
-//	arquivo	teclado.js	-	versão	final
-//	Códigos	de	teclas	-	aqui	vão	todos	os	que	forem	necessários
-var	SETA_ESQUERDA	=	37;
-var	SETA_DIREITA	=	39;
-var	ESPACO	=	32;
-var ENTER = 13;
-var SETA_UP = 40;
-var SETA_DOWN = 38;
+// Códigos de teclas - aqui vão todos os que forem necessários
+var SETA_ESQUERDA = 37;
+var SETA_DIREITA = 39;
+var ESPACO = 32;
 
-function	Teclado(elemento)	{
-			this.elemento	=	elemento;
-			//	Array	de	teclas	pressionadas
-			this.pressionadas	=	[];
-			//	Array	de	teclas	disparadas
-			this.disparadas	=	[];
-			//	Funções	de	disparo
-			this.funcoesDisparo	=	[];
-			var	teclado	=	this;
-			elemento.addEventListener('keydown',	function(evento)	{
-						var	tecla	=	evento.keyCode;		//	Tornando	mais	legível	;)
-						teclado.pressionadas[tecla]	=	true;
-						//	Disparar	somente	se	for	o	primeiro	keydown	da	tecla
-						if	(teclado.funcoesDisparo[tecla]	&&
-										!	teclado.disparadas[tecla])	{
-										teclado.disparadas[tecla]	=	true;
-										teclado.funcoesDisparo[tecla]	()	;
-																}
-			});
-			
-			elemento.addEventListener('keyup',	function(evento)	{
-						teclado.pressionadas[evento.keyCode]	=	false;
-						teclado.disparadas[evento.keyCode]	=	false;
-			});
+function Teclado(elemento) {
+   this.elemento = elemento;
+
+   // Array de teclas pressionadas
+   this.pressionadas = [];
+
+   // Array de teclas disparadas
+   this.disparadas = [];
+
+   // Funções de disparo registradas
+   this.funcoesDisparo = [];
+
+   var teclado = this;
+
+   elemento.addEventListener('keydown', function(evento) {
+      var tecla = evento.keyCode;  // Tornando mais legível ;)
+      teclado.pressionadas[tecla] = true;
+
+      // Disparar somente se for o primeiro keydown da tecla
+      if (teclado.funcoesDisparo[tecla] && !teclado.disparadas[tecla]) {
+
+          teclado.disparadas[tecla] = true;
+          teclado.funcoesDisparo[tecla] () ;
+      }
+   });
+
+   elemento.addEventListener('keyup', function(evento) {
+      teclado.pressionadas[evento.keyCode] = false;
+      teclado.disparadas[evento.keyCode] = false;
+   });
 }
-Teclado.prototype	=	{
-			pressionada:	function(tecla)	{
-						return	this.pressionadas[tecla];
-			},
-			disparou:	function(tecla,	callback)	{
-						this.funcoesDisparo[tecla]	=	callback;
-			}
+Teclado.prototype = {
+   pressionada: function(tecla) {
+      return this.pressionadas[tecla];
+   },
+   disparou: function(tecla, callback) {
+      this.funcoesDisparo[tecla] = callback;
+   }
 }
